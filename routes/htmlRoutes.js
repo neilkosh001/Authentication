0e46df4902e8cdde4416ea43078edd38217caca3
Neil <neilkosh001@gmail.com>
2018-08-25 11:24:15 -0400

    user.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,14 @@ module.exports = function(app) {
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/signup.html"))
     });
+
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"))
+});
+
+app.get("/members", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"))
+});
+
 }
