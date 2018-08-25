752a89c05665e162f845d0df4badafd13732fac5
Neil <neilkosh001@gmail.com>
2018-08-25 10:58:31 -0400

    login page

---
+++ server.js
@@ -0,0 +1,28 @@
+//NPM Packages
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Config
+var PORT    = process.env.port || 8080;
+var mode    = process.env.NODE_ENV;
+var app     = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Routes
+
+require('./routes/htmlRoutes')(app);
+
+
+app.listen(PORT, function(){
+    if(mode !== 'production'){
+        var opn = require('opn')
+        opn(`http://localhost:${PORT}`, {app: ['google chrome']})
+    }
+    console.log(`Hey I'm listening on ${PORT}`);
+})
+
