0e81a43ca5c904ddf736fe27f4581d8b0e110e30
Neil <neilkosh001@gmail.com>
2018-08-25 12:40:15 -0400

    passport.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var passport =  require ('passport');
+var session = require ('express-session');
+
 
 // Config
 var PORT    = process.env.port || 8080;
@@ -13,9 +16,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+//Passport
+app.use(sessionStorage({secret:"helloworld", resave:true, saveUnitialized:true}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 
 require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 
 app.listen(PORT, function(){
